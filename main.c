4ae3b992aef591973af93b27b0077d8fc71c4d92
Roberto Andres Ruiz Pereira <rpruiz33@gmail.com>
2025-08-08 15:20:56 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+
+    for (int i=0;i<6;i++){
+            printf("\n");
+        for(int h=i+1;h<6;h++){
+            printf("*");
+        }
+
+    }
+
+        printf("\n");
+
+
+
+    for(int i=0;i<5;i++){
+        for(int j=0;j<i;j++){
+            printf(" ");
+
+        }
+        printf("*\n");
+}
+
+
+
+
+
+    return 0;
+
+}
